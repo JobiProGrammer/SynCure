f582be426b20b23fb8943410d459e8adb50f031d
mikonse <wtb-123@t-online.de>
2017-04-29 11:24:07 +0200

    First classes added

---
+++ src/syncure/core/FileManager.java
@@ -0,0 +1,7 @@
+package syncure.core;
+
+/**
+ * Created by mikonse on 29.04.2017.
+ */
+public class FileManager {
+}
---
+++ src/syncure/gui/Gui.java
@@ -0,0 +1,7 @@
+package syncure.gui;
+
+/**
+ * Created by mikonse on 29.04.2017.
+ */
+public class Gui {
+}
