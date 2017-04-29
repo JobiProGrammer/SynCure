a3d255750adec76522b232080264a472a6312838
JobiProGrammer <tobias@juelg.net>
2017-04-29 19:37:56 +0200

    New MataData, gerngeschehen ari :+1:

---
+++ src/syncure/core/MetaFileObject.java
@@ -0,0 +1,10 @@
+package syncure.core;
+
+public class MetaFileObject {
+	public String path;
+	public long time;
+	public MetaFileObject(String path, long time){
+		this.path=path;
+		this.time=time;
+	}
+}
