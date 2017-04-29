2396e7efeec052789d49057f9e64c905d7db10e4
mikonse <wtb-123@t-online.de>
2017-04-29 16:45:00 +0200

    FileManager and Main method Threading added.

--- src/syncure/Main.java
+++ src/syncure/Main.java
@@ -1,11 +1,21 @@
 package syncure;
 
+import syncure.core.Config;
+import syncure.core.FileManager;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 /**
  * Created by mikonse on 29.04.2017.
  */
 public class Main {
 
     public static void main(String[] args) {
+        Config config = new Config(null);
+        FileManager manager = new FileManager(config);
 
+        Thread managerThread = new Thread(manager);
+        managerThread.start();
     }
 }
--- src/syncure/core/FileManager.java
+++ src/syncure/core/FileManager.java
@@ -1,7 +1,49 @@
 package syncure.core;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.StandardWatchEventKinds;
+import java.util.ArrayList;
+
 /**
  * Created by mikonse on 29.04.2017.
  */
-public class FileManager {
+public class FileManager implements Runnable{
+
+    private Tree local;
+    private Tree remote;
+    private Object lock = new Object();
+    private Config config;
+
+    public FileManager(Config config) {
+        this.config = config;
+        local = new Tree(config.getLocalDirectory(), lock);
+        remote = new Tree(config.getDriveDirectory(), lock);
+        Thread localThread = new Thread(local);
+        Thread remoteThread = new Thread(remote);
+
+        localThread.start();
+        remoteThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                lock.wait();
+            } catch (InterruptedException e) {
+
+            }
+            ArrayList<File> sources = local.compare(remote);
+            ArrayList<File> targets = remote.compare(local);
+            sync(sources, targets);
+        }
+    }
+
+    public void sync(ArrayList<File> sources, ArrayList<File> targets, boolean syncAll) {
+
+    }
 }
