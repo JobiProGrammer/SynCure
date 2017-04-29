8ee378a0b63497fecff1f73e0a0d4bae07750cc4
schokohoernchen <clemens.horn@tum,de>
2017-04-29 23:14:48 +0200

    added button functionality

--- src/syncure/gui/ConfigGUI.java
+++ src/syncure/gui/ConfigGUI.java
@@ -1,14 +1,19 @@
 package syncure.gui;
+import syncure.core.Config;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.file.FileSystems;
 
 /**
  * Minimal GUI for setting your password, local directory path and online drive path.
  * Offers no methods.
  */
-public class ConfigGUI extends JPanel{
+public class ConfigGUI extends JPanel implements ActionListener{
 
-    //private Config config;
+    private Config config;
     JButton b = new JButton("Test");
     JLabel label1 = new JLabel("Password");
     JLabel label2 = new JLabel("Local directory path");
@@ -20,6 +25,7 @@ public class ConfigGUI extends JPanel{
 
     JButton button = new JButton("Save changes");
     GridBagConstraints c = new GridBagConstraints();
+
 
     public ConfigGUI(){
         super(new GridBagLayout());
@@ -40,5 +46,17 @@ public class ConfigGUI extends JPanel{
         c.gridy = 3;
         c.gridx = 0;
         this.add(button, c);
+        button.addActionListener(this);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(textField1.getText().length() > 1)
+            config.setPassword(textField1.getText());
+        if(textField2.getText().length() > 1)
+            config.setLocalDirectory(FileSystems.getDefault().getPath(textField2.getText()));
+        if(textField3.getText().length() > 1)
+            config.setDriveDirectory(FileSystems.getDefault().getPath(textField3.getText()));
+    }
+
 }
