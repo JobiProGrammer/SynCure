d814b8b08eb7deb7c6526a8ddebc3c2122d4043f
schokohoernchen <clemens.horn@tum,de>
2017-04-29 19:56:46 +0200

    no functionality still

--- src/syncure/gui/ConfigGUI.java
+++ src/syncure/gui/ConfigGUI.java
@@ -10,14 +10,15 @@ public class ConfigGUI extends JFrame{
     //private Config config;
     JPanel p = new JPanel(new GridBagLayout());
     JButton b = new JButton("Test");
-    JLabel label1 = new JLabel("Label 1");
-    JLabel label2 = new JLabel("Label 2");
-    JLabel label3 = new JLabel("Label 3");
+    JLabel label1 = new JLabel("Password");
+    JLabel label2 = new JLabel("Local directory path");
+    JLabel label3 = new JLabel("Online drive directory path");
 
     JTextField textField1 = new JTextField(10);
     JTextField textField2 = new JTextField(10);
     JTextField textField3 = new JTextField(10);
 
+    JButton button = new JButton("Save changes");
     GridBagConstraints c = new GridBagConstraints();
 
     public static void main(String[] args)
@@ -30,11 +31,22 @@ public class ConfigGUI extends JFrame{
 
         setSize(300, 400);
         setResizable(true);
-        p.add(label1);
-        p.add(textField1);
-        p.add(label2);
-        p.add(textField2);
-        p.add(label3);
+        c.gridy = 0;
+        label1.setHorizontalAlignment(SwingConstants.RIGHT);
+        p.add(label1, c);
+        p.add(textField1, c);
+
+        c.gridy = 1;
+        p.add(label2, c);
+        p.add(textField2, c);
+
+        c.gridy = 2;
+        p.add(label3, c);
+        p.add(textField3, c);
+
+        c.gridy = 3;
+        c.gridx = 0;
+        p.add(button, c);
         add(p);
 
 
